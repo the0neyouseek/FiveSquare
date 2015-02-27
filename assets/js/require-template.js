839fc0f19205df468a2bf7b2c3f84e70c2b877bf
the0neyouseek <iamnotheoneyouseek@gmail.com>
2015-02-27 08:30:35 +0100

    [RequireJs] Template de module
    Création d'un template de module RequireJs
    + require-template.js
    
    Signed-off-by: the0neyouseek <iamnotheoneyouseek@gmail.com>

---
+++ assets/js/require-template.js
@@ -0,0 +1,13 @@
+// Fichier Definition de Module RequireJs
+define(['jquery'], function($) {
+
+    var initialize = function() {
+        // Fonctions a executer une fois le module chargé & initialize() appelé.
+    };
+
+    return {
+        initialize: initialize
+        // Renvoi la fonction initialize
+        // Pour l'utiliser dans d'autres modules
+    };
+});
